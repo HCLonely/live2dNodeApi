16865380fade86a41992a9ad1a9682694d795bb6
lbr77 <2298454647@qq.com>
2021-08-30 20:57:10 +0800

    Modify

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 /*
  * @Date: 2021-08-30 20:41:14
  * @LastEditors: Steve Li
- * @LastEditTime: 2021-08-30 20:41:15
+ * @LastEditTime: 2021-08-30 20:56:03
  * @FilePath: \live2dNodeApi\main.js
  */
-require('./app.js')
+const app = require('./app.js');
+
+app.server = app.listen(3000,'',()=>{
+    console.log(`running on 3000`);
+})
