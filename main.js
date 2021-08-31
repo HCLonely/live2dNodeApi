f6889feb5f13b1d6d27c828342f6765b82495015
HCLonely <h1606051253@gmail.com>
2021-08-31 21:12:27 +0800

    vercel部署优化

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 /*
  * @Date: 2021-08-30 20:41:14
  * @LastEditors: HCLonely
- * @LastEditTime: 2021-08-31 18:35:34
+ * @LastEditTime: 2021-08-31 18:45:15
  * @FilePath: \live2dNodeApi\main.js
  */
 const app = require('./app.js')
 
-app.server = app.listen(3000, '', () => {
-  console.log('running on 3000')
+app.server = app.listen(8080, '', () => {
+  console.log('running on 8080')
 })
