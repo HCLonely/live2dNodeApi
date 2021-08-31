61f68112ac905fe7eaa2379d8ed6cf94711ebb7a
HCLonely <h1606051253@gmail.com>
2021-08-31 17:34:52 +0800

    vercel部署优化

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 /*
  * @Date: 2021-08-30 20:41:14
  * @LastEditors: HCLonely
- * @LastEditTime: 2021-08-31 17:20:30
+ * @LastEditTime: 2021-08-31 17:33:45
  * @FilePath: \live2dNodeApi\main.js
  */
 const app = require('./app.js')
 
-module.exports = (req, res) => {
-  app.callback()(req, res)
-}
+app.server = app.listen(2333, '', () => {
+  console.log('running on 2333')
+})
