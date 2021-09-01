59bfb753373abed1643edc882d64bf3333b5bc20
HCLonely <h1606051253@gmail.com>
2021-09-01 15:09:08 +0800

    Vercel部署优化

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: HCLonely
  * @Date: 2021-09-01 12:42:09
- * @LastEditTime: 2021-09-01 14:52:43
+ * @LastEditTime: 2021-09-01 15:09:05
  * @LastEditors: HCLonely
  * @FilePath: \live2dNodeApi\routes\index.js
  * @Description:
@@ -11,7 +11,7 @@ const router = express.Router()
 const path = require('path')
 
 router.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../html/index.html'))
+  res.sendFile(path.join(__dirname, '../static/index.html'))
 })
 
 module.exports = router
